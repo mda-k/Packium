b568a4d882ff4425ac79e0a1c9aef73ba99cbb35
mda-k <dajka320@gmail.com>
2026-07-22 00:07:39 +0300

    some minor adjustments

--- ui/one.py
+++ ui/one.py
@@ -1,5 +1,6 @@
 import customtkinter as ctk
 import tkinter as tk
+import pywinstyles
 def mainui():
     app = ctk.CTk()
     app.geometry("400x200")
@@ -12,15 +13,15 @@ def mainui():
         app.after(100, app.destroy())
     app.protocol("WM_DELETE_WINDOW", FUCKMYLIFE)
     app.title("Winget UI")
-    app.attributes("-alpha", 0.7)
     app.attributes("-topmost", True)
     transparent_color = "#000001"
+    app.attributes("-alpha", 0.7)
     app.attributes("-transparentcolor", transparent_color)
     app.configure(fg_color=transparent_color)
     overlayframe = ctk.CTkFrame(app, corner_radius=25)
     overlayframe.pack(fill="both", expand=True, padx=2, pady=2)
-    close = ctk.CTkButton(overlayframe, text="", fg_color="gray", hover_color="white", width=10, height=10, corner_radius=20, command=lambda:FUCKMYLIFE())
+    close = ctk.CTkButton(overlayframe, text="", fg_color="white", hover_color="gray", width=8, height=8, corner_radius=4, command=lambda:FUCKMYLIFE())
     close.place(relx=1.0, rely=0.0, x=-12, y=12, anchor="ne")
-    name = ctk.CTkLabel(overlayframe, text="Winget UI", fg_color="transparent", font=("Arial", 16))
-    name.pack(side="top", pady=(10, 0))
+    name = ctk.CTkLabel(overlayframe, text="Packium", fg_color="transparent", font=("Arial", 16, "bold"), text_color="white")
+    name.pack(side="top", pady=(2, 12))
     app.mainloop()
